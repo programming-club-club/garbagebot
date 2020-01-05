9c2a8be0ed06f2b5669d142c6673ad8691b0d2ec
Sam <43186813+oni7@users.noreply.github.com>
2020-01-05 15:00:09 +0000

    Added stuff
    the bot now swears back at you

--- bot.js
+++ bot.js
@@ -1,12 +1,14 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
+
 // Initialize Discord Bot
 var bot = new Discord.Client({
    token: auth.token,
@@ -17,68 +19,70 @@ bot.on('ready', function (evt) {
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
+
+const commands = {
+	"ping": ping,
+	"stinky": stinky,
+	"info": info,
+	"help": help,
+};
+
+const blacklist = ['fuck', 'shit', 'ass', 'cuck'];
+
+function sendMessage(channel, message) {
+    bot.sendMessage({to: channel, message: message});
+};
+
+function info(channelID){
+	sendMessage(channelID, '```Hello! I am a bot. So far I don`t do much, but ' +
+	'I`ve been made by: \n' +
+	'\nRENDOSKI#3031 \n' +
+	'\nstart by getting a list of commands, using !help```');
+};
+
+// Command list.
+function help(channelID){
+	sendMessage(channelID,'```List of current working commands: \n' +
+	'- !stinky \n' +
+	'- !info \n' +
+	'- !help \n' +
+	'- !ping \n```');
+};
+
+function ping(channelID){
+	sendMessage(channelID, 'pong!');
+};
+
+function stinky(channelID){
+	sendMessage(channelID, 'go take a shower, smelly!');
+};
+
+// this doesn't work cause im stupid
+/*function repeat(channelID, message)
+{
+	if (blacklist.includes(message))
+		{
+		sendMessage(channelID, message);
+		}
+};*/
+
 bot.on('message', function (user, userID, channelID, message, evt) {
+	
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
 	
-	/* Planned functions: 
-	!bruh (posts a bruh sound effect idk)
-	!yt 'whatever' (searches for a youtube video)
-	!google 'blah blah' (posts a google link to a search for the input maybe)
-	!uwuize 'sentence' (you know what the fuck this thing does)
-	!hydration (toggle command maybe? reminder to drink water or some gay shit)
-	*/
+	if (message.substring(0, 1) == '!'){
+		var args = message.substring(1).split(' ');
+		var cmd = args[0];
+		
+		if (commands[cmd]) {
+			commands[cmd](channelID);
+		}
+	}
 	
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-			// Adding commands requires 
-			/* 	
-				case 'newCommand':
-				bot.sendMessage({ to: channelID, message: 'responce to new command' });
-				break; 
-			*/
-			
-			// Command !stinky just insults the user.
-			// Maybe when given a ping, it can insult someone else?
-			case 'stinky':
-				bot.sendMessage({ 
-					to: channelID, 
-					message: 'go take a shower, smelly!' 
-				});
-			break;
-			
-			// Command !info tells the user about the bot's capabilities.
-			case 'info':
-			bot.sendMessage({ 
-				to: channelID, 
-				message: '```Hello! I am a bot. So far I don`t do much, but ' +
-				'I`ve been made by: \n' +
-				'\n RENDOSKI#3031 \n' +
-				'\nstart by getting a list of commands, using !commands```'
-			});
-			break;
-			
-			case 'commands':
-			bot.sendMessage({
-				to: channelID,
-				message: '```List of current working commands: \n' +
-				'- !stinky \n' +
-				'- !info \n' +
-				'- !commands \n```'
-			});
-			break;
-         }
-     }
+	for (let word of message.split(' ')){
+		if (blacklist.includes(word.toLowerCase())){
+			sendMessage(channelID, word + '!');
+		}
+	}
 });
